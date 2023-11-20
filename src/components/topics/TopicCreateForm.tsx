5162365f901b8caa81289981954202b808f89f50
IvanOrsh <ksironov@mail.ru>
2023-11-21 00:22:15 +0300

    feat: topic create form ui showing errors

--- src/components/topics/TopicCreateForm.tsx
+++ src/components/topics/TopicCreateForm.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useFormState } from "react-dom";
 import {
   Input,
   Button,
@@ -10,21 +13,40 @@ import {
 import { createTopic } from "@/actions";
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(createTopic, { errors: {} });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={createTopic}>
+        <form action={action.bind(formState)}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
+            />
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
+
+            {formState.errors._form ? (
+              <div className="p-2 bg-red-200 border-red-400 rounded-lg">
+                {formState.errors._form?.join(", ")}
+              </div>
+            ) : null}
+
             <Button type="submit">Submit</Button>
           </div>
         </form>
