3d314245c803b885550140bc11355b7a0bfd1308
IvanOrsh <ksironov@mail.ru>
2023-11-21 01:07:12 +0300

    chore: scaffold topic page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,7 +1,19 @@
-export default async function TopicPage() {
+type TopicPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function TopicPage({ params }: TopicPageProps) {
+  const { slug } = params;
+
   return (
-    <div>
-      <h1>Topic</h1>
-    </div>
+    <main className="grid grid-cols-4 gap-4 p-4">
+      <article className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </article>
+
+      <div></div>
+    </main>
   );
 }
