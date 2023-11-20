ff88924e9d437b594aeaf08b32a7f7f2d726fe90
IvanOrsh <ksironov@mail.ru>
2023-11-21 01:36:18 +0300

    chore: update paths

--- src/paths.ts
+++ src/paths.ts
@@ -7,9 +7,13 @@ const paths = {
     return `/topics/${topicSlug}`;
   },
 
-  postCreate() {},
+  postCreate(topicSlug: string) {
+    return `/topics/${topicSlug}/posts/new`;
+  },
 
-  postShow() {},
+  postShow(topicSlug: string, postId: string) {
+    return `/topics/${topicSlug}/posts/${postId}`;
+  },
 };
 
 export default paths;
